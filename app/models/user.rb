efddff41d8e86449742a53d4eb7e9534367605a7
AmirolAhmad <9w2pdf@gmail.com>
2014-08-25 07:09:26 +0800

    change the method to upload the avatar

--- app/models/user.rb
+++ app/models/user.rb
@@ -5,12 +5,7 @@ class User < ActiveRecord::Base
   devise :database_authenticatable, :registerable,
          :recoverable, :rememberable, :trackable, :validatable, :confirmable
 
-  # Setup accessible (or protected) attributes for your model
-  # attr_accessible :email, :password, :remember_me, :avatar, :avatar_cache, :remove_avatar
-
   validates_presence_of   :avatar
-  validates_integrity_of  :avatar
-  validates_processing_of :avatar
 
   scope :admin, -> { where(admin: true) }
 end
