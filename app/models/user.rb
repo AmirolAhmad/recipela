dfc9e5a497a3a2fe7811c5ec0de9f4fbcf467fc6
AmirolAhmad <9w2pdf@gmail.com>
2014-08-25 06:21:40 +0800

    messed up

--- app/models/user.rb
+++ app/models/user.rb
@@ -1,10 +1,14 @@
 class User < ActiveRecord::Base
+  # Avatar uploader using carrierwave
+  mount_uploader :avatar, AvatarUploader
   # Include default devise modules. Others available are:
   # :confirmable, :lockable, :timeoutable and :omniauthable
   devise :database_authenticatable, :registerable,
          :recoverable, :rememberable, :trackable, :validatable, :confirmable
 
   scope :admin, -> { where(admin: true) }
+
+  
 
   # Overridden to notify users with password changes
   def update_with_password(params, *options)
