b3e216192782098b99afdd110e7723c32442754e
AmirolAhmad <9w2pdf@gmail.com>
2014-08-25 00:35:54 +0800

    setup authenticable/admin module

--- app/controllers/greetings_controller.rb
+++ app/controllers/greetings_controller.rb
@@ -1,4 +1,5 @@
 class GreetingsController < ApplicationController
+	before_filter :store_location, only: :hello
   def hello
   end
 end
---
+++ lib/authenticable.rb
@@ -0,0 +1,27 @@
+module Authenticable
+  extend ActiveSupport::Concern
+  included do
+    helper_method :require_admin, :store_location, :redirect_back_or_default, :get_redirect_back_or_default   
+  end
+
+  private
+  def require_admin
+    unless current_user.try(:admin?)
+      flash.notice = "Permissions required to access this page."
+      redirect_to root_path
+    end
+  end
+
+  def store_location
+    session[:return_to] = request.original_url rescue nil 
+  end
+
+  def redirect_back_or_default(default)
+    redirect_to (session[:return_to] || default)
+    session[:return_to] = nil
+  end
+
+  def get_redirect_back_or_default(default)
+    redirect = session[:return_to] ? session[:return_to] : default
+  end
+end
