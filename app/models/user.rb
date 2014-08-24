1d2a784cce93b070c4544f2db026ece24e4df290
AmirolAhmad <9w2pdf@gmail.com>
2014-08-25 00:29:31 +0800

    add admin, devise_mailer, confirm email & notify change password

--- app/models/user.rb
+++ app/models/user.rb
@@ -2,5 +2,15 @@ class User < ActiveRecord::Base
   # Include default devise modules. Others available are:
   # :confirmable, :lockable, :timeoutable and :omniauthable
   devise :database_authenticatable, :registerable,
-         :recoverable, :rememberable, :trackable, :validatable
+         :recoverable, :rememberable, :trackable, :validatable, :confirmable
+
+  scope :admin, -> { where(admin: true) }
+
+  # Overridden to notify users with password changes
+  def update_with_password(params, *options)
+    if super
+      # TODO schedule this in the background
+      UserMailer.password_changed(self.id).deliver
+    end
+  end
 end
