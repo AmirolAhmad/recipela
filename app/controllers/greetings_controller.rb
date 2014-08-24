51f9fb11802c3a8c88f1591186decd54e1aaed3f
AmirolAhmad <9w2pdf@gmail.com>
2014-08-25 03:30:16 +0800

    pages added

--- app/controllers/greetings_controller.rb
+++ app/controllers/greetings_controller.rb
@@ -2,4 +2,28 @@ class GreetingsController < ApplicationController
 	before_filter :store_location, only: :hello
   def hello
   end
+
+  def about
+  end
+
+  def terms
+  end
+
+  def privacy
+  end
+
+  def cookie
+  end
+
+  def advertising
+  end
+
+  def developers
+  end
+
+  def help
+  end
+
+  def contact
+  end
 end
--- config/routes.rb
+++ config/routes.rb
@@ -1,5 +1,13 @@
 Rails.application.routes.draw do
   root to: 'greetings#hello'
+  get 'about', to: "greetings#about"
+  get 'terms', to: "greetings#terms"
+  get 'privacy', to: "greetings#privacy"
+  get 'cookies', to: "greetings#cookie"
+  get 'advertising', to: "greetings#advertising"
+  get 'developers', to: "greetings#developers"
+  get 'help', to: "greetings#help"
+  get 'contact', to: "greetings#contact"
 
   devise_for :users
   devise_scope :user do
