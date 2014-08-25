73e6e80101bae7140408ef4a1ab7b603bb30413b
AmirolAhmad <9w2pdf@gmail.com>
2014-08-25 08:50:27 +0800

    remove

--- config/initializers/carrierwave.rb
+++
@@ -1,8 +0,0 @@
-CarrierWave.configure do |config| 
-  config.fog_credentials = { 
-    :provider               => 'AWS', 
-    :aws_access_key_id      => 'AKIAJGCAP5DNO5ITBSPQ', 
-    :aws_secret_access_key  => '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', 
-  } 
-  config.fog_directory  = 'recipela'
-end 
